d2ca4baaa8f3356519c29bdec1d719e374dd2873
XrayHunter <51248011+XrayHunter@users.noreply.github.com>
2023-01-18 12:51:51 +0100

    Update TH55.sh

--- TH55.sh
+++ TH55.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ./nextjtag -a -m  -B --set-voltage=0.65 --disable-voltage-limit  
+sudo ./nextjtag -a -m  -B --set-voltage=0.60 --disable-voltage-limit  
 sudo ./teamredminer -a kas -o stratum+tcp://pool.woolypooly.com:3112 -u kaspa:qzhfmet4u7geehex58s8lz4yxxcjujzem9fzjqgfusc08q9mdmze625y655kq.podnik -p x --fpga_tmem_limit=80 --fpga_clk_core=610 --fpga_tcore_limit=70 --hardware=fpga
